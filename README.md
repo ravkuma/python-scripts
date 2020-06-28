17bc2cb4315e6e4d024aa90e995f68ea7e98fcd0
Ravikumar Raju <raraju@raraju-a02.vmware.com>
2020-06-28 12:20:50 +0530

    Adding Bubble Sort

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# python-scripts
+This is my repository to store my python scripts which 
+I'm creating as part of my Python Course.
---
+++ bubble_sort.py
@@ -0,0 +1,31 @@
+def bs(a):
+    b=len(a)-1
+    for x in range(b):
+        for y in range(b-x):
+            if a[y]>a[y+1]:
+                a[y],a[y+1]=a[y+1],a[y]
+    return a
+
+
+## Main
+
+print("This is a Bubble Sort")
+
+# creating an empty list 
+lst = [] 
+  
+# number of elemetns as input 
+n = int(input("Enter number of elements : ")) 
+  
+# iterating till the range 
+print("Enter %d numbers one by one" %n)
+
+for i in range(0, n): 
+    ele = int(input()) 
+  
+    lst.append(ele) # adding the elemenat
+
+print("List to sort ",lst) 
+
+print("Sorted List:", bs(lst))
+
